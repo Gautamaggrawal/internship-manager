4d2ebccc9c90e784b81688c378538faa63ecbe8e
Gautam <gautamaggrawalsd@yahoo.in>
2019-04-07 23:57:18 +0530

    static

--- internshipmanager/urls.py
+++ internshipmanager/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name="login"),
     path('signup/',SignUpView.as_view(), name="signup"),
     path('', login_required(InternsListView.as_view(),login_url='login'), name="home"),
-    path('interndetails/<int:pk>', staff_member_required(InternDetailView.as_view(),login_url='login'), name='intern-detail'),
+    path('interndetails/<int:pk>', login_required(InternDetailView.as_view(),login_url='login'), name='intern-detail'),
     path('interncreate/', login_required(InternCreateView.as_view(),login_url='login'), name='intern-create'),
     path('internupdate/<int:pk>', login_required(InternUpdateView.as_view(),login_url='login'), name='intern-update'),
     path('interndelete/<int:pk>', login_required(InternDeleteView.as_view(),login_url='login'), name='intern-delete'),
