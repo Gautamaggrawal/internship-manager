259bfc85f1c2b22ccaf81f488a51b02695654770
Gautam <gautamaggrawalsd@yahoo.in>
2019-04-07 23:25:42 +0530

    static

--- internshipmanager/settings.py
+++ internshipmanager/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
 ]
 # location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Static files (CSS, JavaScript, Images)
