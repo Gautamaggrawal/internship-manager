b45a3b896175418cb35a58d356db3d423f9eb549
Gautam <gautamaggrawalsd@yahoo.in>
2019-04-07 23:03:57 +0530

    heroku

--- internshipmanager/settings.py
+++ internshipmanager/settings.py
@@ -129,13 +129,16 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
 ]
 # location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+# STATIC_ROOT = '/static/'
+# STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 LOGIN_REDIRECT_URL='/'
